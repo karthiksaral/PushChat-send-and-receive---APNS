294764c19684098bb9a8256d3ff65450e3038fda
Kauserali Hafizji <ali.hafizji@gmail.com>
2013-03-28 12:06:23 +0530

    Initial Commit

---
+++ PushChatStarter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PushChatStarter
+//
+//  Created by Kauserali on 28/03/13.
+//  Copyright (c) 2013 Ray Wenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
